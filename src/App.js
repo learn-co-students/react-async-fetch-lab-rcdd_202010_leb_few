85c600ed6f634d06226c9ae3befa9cd1d2ec0726
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-12 17:19:37 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 class App extends React.Component{
+  state={
+    people: []
+  }
   componentDidMount(){
     fetch(`http://api.open-notify.org/astros.json`)
     .then(res => res.json())
-    .then(data => data.people)
+    .then(data => this.setPeople(data.people))
   }
   render(){
-    return(null)
+    return(
+      <div> {this.state.people.map((person, id) => <h1 key={id}>{person.name}</h1>)}</div>
+    )
   }
 }
 export default App
